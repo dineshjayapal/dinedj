822c66184081c3b10995c7f443e98bbca1ca3512
dineshjayapal <dineshjayapal97@gmail.com>
2018-05-29 21:43:50 -0700

    switch case

---
+++ swtcase.java
@@ -0,0 +1,18 @@
+public class swtcase
+{public static void main(String args[])
+{
+int choice=2; 
+System.out.println(choice);
+switch(choice)
+{
+case 1:
+System.out.println("hai");break;
+case 2:
+System.out.println("hello");break;
+case 3:
+System.out.println("world");break;
+default:
+break;
+}
+}
+}
