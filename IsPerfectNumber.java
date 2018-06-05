0a67f30806029752dd2a339db522e4ebc23ef5b3
dineshjayapal <dineshjayapal97@gmail.com>
2018-06-05 12:44:10 -0700

    Perfect

---
+++ IsPerfectNumber.java
@@ -0,0 +1,23 @@
+package com.basic;
+
+public class IsPerfectNumber {
+	public boolean isPerfectNumber(int number) {
+
+		int temp = 0;
+		for (int i = 1; i <= number / 2; i++) {
+			if (number % i == 0) {
+				temp += i;
+			}
+		}
+		if (temp == number) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void main(String a[]) {
+		IsPerfectNumber ipn = new IsPerfectNumber();
+		System.out.println("Is perfect number: " + ipn.isPerfectNumber(467));
+	}
+}
