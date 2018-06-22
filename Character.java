0181285cb06ed0417b01aade2e69cf839a42c6a9
dineshjayapal <dineshjayapal97@gmail.com>
2018-06-22 15:09:16 +0530

    Character

---
+++ Character.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+public class Character
+{
+  public static void main(String[] args)
+     {
+	char ch='*';
+        if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+          {  System.out.println("Yes");  }
+        else
+          {  System.out.println("No");  }
+      }
+}	
