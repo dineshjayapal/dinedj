867bdaf29b4d509bc66a3d3e8eb8878a8c3646aa
dineshjayapal <dineshjayapal97@gmail.com>
2018-06-22 15:18:00 +0530

    count

---
+++ Count2.java
@@ -0,0 +1,16 @@
+public class Count2
+{
+  public static void main(String[] args)
+     {
+	int count = 0, num = 3452;
+
+        while(num != 0)
+        {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println(count);
+      }
+}	
